e2b82007026e0d1276b06e4e65691b3353647350
Avgust work pc <avgustfavn@yandex.ru>
2024-06-27 08:47:12 +0300

    Parser final

---
+++ app.py
@@ -0,0 +1,201 @@
+import xml.etree.ElementTree as ET
+from models import Offer, Task
+from sqlalchemy.orm import Session
+from flask import Flask, Response, request, render_template, abort, redirect
+from engine import engine
+import hashlib
+
+app = Flask(__name__)            
+
+def ishash(check: str) -> bool:
+    with open('creds.txt','r') as file:
+        based = file.read().split('\n')
+    original = hashlib.sha256(''.join(based).encode()).hexdigest()
+    if original == check:
+        return True
+
+@app.post('/task')
+def newtask():
+    token = request.cookies.get('token')
+    if token and ishash(token):
+        time = request.form.get('time')
+        day = request.form.get('weekday')
+        if time and day:
+            with Session(engine) as session:
+                task = Task(weekday=int(day),time=str(time))
+                session.add(task)
+                session.commit()
+        return redirect('/')
+    else:
+        return redirect('/login')
+    
+@app.post('/delete')
+def deltask():
+    token = request.cookies.get('token')
+    if token and ishash(token):
+        index = request.form.get('index')
+        with Session(engine) as session:
+            obj = session.query(Task).where(Task.id == index).scalar()
+            session.delete(obj)
+            session.commit()
+        return redirect('/')
+    else:
+        return redirect('/login')
+    
+    
+
+@app.post('/login')
+def dologin():
+    login = request.form.get('login')
+    password = request.form.get('password')
+    if ishash(hashlib.sha256((str(login+password).encode())).hexdigest()):
+        resp = redirect('/')
+        resp.set_cookie('token',hashlib.sha256(str(login+password).encode()).hexdigest())
+        return resp
+    return abort(401)
+    
+
+@app.get('/login')
+def relogin():
+    return render_template('login.html')
+
+@app.get('/')
+def panel():
+    token = request.cookies.get('token')
+    if token and ishash(token):
+        weekdays = ['Понедельник','Вторник','Среда','Четверг','Пятница','Суббота','Воскресенье','Каждый день']
+        with Session(engine) as session:
+            tasks:list[Task] = session.query(Task).all()
+            tasks = [[i.id,weekdays[i.weekday],i.time] for i in tasks]
+            return render_template('panel.html',tasks = tasks)
+    else:
+        return redirect('/login')
+
+@app.get('/download')
+def downloadxml():
+    with Session(engine) as session:
+        elements: list[Offer] = session.query(Offer).all() 
+        data = ET.Element('offers')
+        for i in elements:
+            if i.barcode != '' and i.vendorarticle != '':
+                base = ET.SubElement(data,'offer')
+                
+                sku = ET.SubElement(base,'sku')
+                vendorarticle = ET.SubElement(base,'vendorarticle')
+                vendor = ET.SubElement(base,'vendor')
+                name = ET.SubElement(base,'name')
+                url = ET.SubElement(base,'url')
+                price = ET.SubElement(base,'price')
+                barcode = ET.SubElement(base,'barcode')
+                available = ET.SubElement(base,'available')
+                description = ET.SubElement(base,'description')
+                pictures = ET.SubElement(base,'picture')
+                
+                url.text = str(i.url)
+                name.text = str(i.name)
+                sku.text = str(i.sku)
+                price.text = str(i.varprice)
+                available.text = str(int(bool(i.stock)))
+                barcode.text = str(i.barcode)
+                vendorarticle.text = str(i.vendorarticle)
+                vendor.text = str(i.vendor)
+                description.text = str(i.description)
+                pictures.text = str(i.images)
+        resp = Response(ET.tostring(data, encoding='utf8'),mimetype='text/xml')
+        resp.headers['Content-Disposition'] = 'attachment'
+        return resp
+    
+@app.get('/download-min')
+def downloadminxml():
+    with Session(engine) as session:
+        elements: list[Offer] = session.query(Offer).all() 
+        data = ET.Element('offers')
+        for i in elements:
+            if i.barcode != '' and i.vendorarticle != '':
+                base = ET.SubElement(data,'offer')
+                
+                sku = ET.SubElement(base,'sku')
+                vendorarticle = ET.SubElement(base,'vendorarticle')
+                vendor = ET.SubElement(base,'vendor')
+                name = ET.SubElement(base,'name')
+                url = ET.SubElement(base,'url')
+                price = ET.SubElement(base,'price')
+                barcode = ET.SubElement(base,'barcode')
+                available = ET.SubElement(base,'available')
+                
+                url.text = str(i.url)
+                name.text = str(i.name)
+                sku.text = str(i.sku)
+                price.text = str(i.varprice)
+                available.text = str(int(bool(i.stock)))
+                barcode.text = str(i.barcode)
+                vendorarticle.text = str(i.vendorarticle)
+                vendor.text = str(i.vendor)
+        resp = Response(ET.tostring(data, encoding='utf8'),mimetype='text/xml')
+        resp.headers['Content-Disposition'] = 'attachment'
+        return resp
+
+@app.get('/show')
+def showxml():
+    with Session(engine) as session:
+        elements: list[Offer] = session.query(Offer).all() 
+        data = ET.Element('offers')
+        for i in elements:
+            if i.barcode != '' and i.vendorarticle != '':
+                base = ET.SubElement(data,'offer')
+                
+                sku = ET.SubElement(base,'sku')
+                vendorarticle = ET.SubElement(base,'vendorarticle')
+                vendor = ET.SubElement(base,'vendor')
+                name = ET.SubElement(base,'name')
+                url = ET.SubElement(base,'url')
+                price = ET.SubElement(base,'price')
+                barcode = ET.SubElement(base,'barcode')
+                available = ET.SubElement(base,'available')
+                description = ET.SubElement(base,'description')
+                pictures = ET.SubElement(base,'picture')
+                
+                url.text = str(i.url)
+                name.text = str(i.name)
+                sku.text = str(i.sku)
+                price.text = str(i.varprice)
+                available.text = str(int(bool(i.stock)))
+                barcode.text = str(i.barcode)
+                vendorarticle.text = str(i.vendorarticle)
+                vendor.text = str(i.vendor)
+                description.text = str(i.description)
+                pictures.text = str(i.images)
+        resp = Response(ET.tostring(data, encoding='utf8'),mimetype='text/xml')
+        return resp
+    
+@app.get('/show-min')
+def showminxml():
+    with Session(engine) as session:
+        elements: list[Offer] = session.query(Offer).all() 
+        data = ET.Element('offers')
+        for i in elements:
+            if i.barcode != '' and i.vendorarticle != '':
+                base = ET.SubElement(data,'offer')
+                
+                sku = ET.SubElement(base,'sku')
+                vendorarticle = ET.SubElement(base,'vendorarticle')
+                vendor = ET.SubElement(base,'vendor')
+                name = ET.SubElement(base,'name')
+                url = ET.SubElement(base,'url')
+                price = ET.SubElement(base,'price')
+                barcode = ET.SubElement(base,'barcode')
+                available = ET.SubElement(base,'available')
+                
+                url.text = str(i.url)
+                name.text = str(i.name)
+                sku.text = str(i.sku)
+                price.text = str(i.varprice)
+                available.text = str(int(bool(i.stock)))
+                barcode.text = str(i.barcode)
+                vendorarticle.text = str(i.vendorarticle)
+                vendor.text = str(i.vendor)
+        resp = Response(ET.tostring(data, encoding='utf8'),mimetype='text/xml')
+        return resp
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0',port='2500')
---
+++ lookup.py
@@ -0,0 +1,195 @@
+from models import Offer
+from random import choice
+from fake_useragent import FakeUserAgent
+from sqlalchemy.orm import Session
+from engine import engine
+import requests as rs
+from bs4 import BeautifulSoup
+import threading
+import socket
+import ujson
+import time
+import re
+import traceback
+from http import cookiejar
+from random import uniform
+
+class BlockAll(cookiejar.CookiePolicy):
+    return_ok = set_ok = domain_return_ok = path_return_ok = lambda self, *args, **kwargs: False
+    netscape = True
+    rfc2965 = hide_cookie2 = False
+
+ports = []
+links = []
+overflow = 0
+
+requests = rs.Session()
+requests.cookies.set_policy(BlockAll())
+
+def getdescription(soup: BeautifulSoup) -> str:
+    result = []
+    i: BeautifulSoup
+    for i in soup.find_all('div',class_='col-xs-24'):
+        temp = [i for i in str(i).strip().split('\n') if i != '']
+        result.append('\n'.join(temp))
+    return ''.join(result)
+    
+def getimages(soup: BeautifulSoup) -> str:
+    result = []
+    i: BeautifulSoup
+    for i in soup.find_all('div',class_='carousel__slide imgvisible',attrs={'data-fancybox':'gallery','data-src':True}):
+        result.append(i.get('data-src'))
+    return ','.join(result)
+    
+
+def randproxy() -> str:
+    global ports
+    return str(choice(ports))
+
+def getinfo(url:str) -> dict:
+    start = time.time()
+    while True:
+        try:
+            if int(time.time() - start) >= 300:
+                return False
+            proxy = randproxy()
+            
+            proxies = {
+                "http"  : 'http://127.0.0.1:'+proxy,
+                "https" : 'http://127.0.0.1:'+proxy
+                }
+            r = requests.get(url=url,proxies=proxies,headers={'User-Agent': FakeUserAgent(os=['windows']).chrome,'Referer':'https:/yandex.ru/'},timeout=9)
+            soup = BeautifulSoup(r.text,'html.parser')
+            
+            sku = soup.find('h1',attrs={'itemprop':'name','data-product':True})
+            if sku:
+                sku = sku['data-product']
+            else:
+                sku = ''
+            
+            name = soup.find('meta',attrs={'itemprop':'name'})
+            if name:
+                name = name['content']
+            else:
+                name = ''
+                
+            stock = soup.find('meta',attrs={'itemprop':'availability'})
+            if stock and stock['content'] == 'InStock' :
+                stock = True
+            else:
+                stock = False
+            
+            vendor = soup.find('a',class_='nonelink',attrs={'title':True,'href':True})
+            if vendor:
+                vendor = vendor['title']
+            else:
+                vendor = ''
+            
+            vendorarticle = soup.find('meta',attrs={'itemprop':'sku'})
+            if vendorarticle:
+                vendorarticle = vendorarticle['content']
+            else:
+                vendorarticle = ''
+            
+            varprice = soup.find('span',class_='price')
+            if varprice:
+                    varprice = int(varprice.text.replace(' ',''))
+            else:
+                varprice = 0
+            
+            cprice = varprice
+            
+            barcode = soup.find('label',class_='lfeature',attrs={'id':re.compile('product_.*?_feature_102')})
+            if barcode:
+                barcode = barcode.text
+            else:
+                barcode = ''
+                
+            imgs = getimages(soup)
+            desc = getdescription(soup)
+            print(f'in {time.time() - start} sec ended Thread with {url}',flush=True)
+            if sku:
+                return url,name,sku,stock,varprice,cprice,barcode,vendor,vendorarticle,imgs,desc
+            else:
+                raise Exception('No sku found')
+        except:
+            pass
+            
+def newinfo(url:str) -> None:
+    global overflow
+    overflow += 1
+    while True:
+        try:
+            info = getinfo(url=url)
+            if info == False:
+                return None
+            with Session(engine) as session:
+                of: Offer = session.query(Offer).where(Offer.url == info[0]).scalar()
+                if of:
+                    of.name = info[1]
+                    of.sku = info[2]
+                    of.stock = info[3]
+                    of.varprice = info[4]
+                    of.cprice = info[5]
+                    of.barcode = info[6]
+                    of.vendor = info[7]
+                    of.vendorarticle = info[8]
+                    of.images = info[9]
+                    of.description = info[10]
+                else:
+                    offer = Offer(url = info[0], name= info[1], sku = info[2], stock = info[3], varprice = info[4], cprice = info[5], barcode = info[6], vendor = info[7], vendorarticle = info[8], images = info[9], description = info[10])
+                    session.add(offer)
+                print(info,flush=True)
+                session.commit()
+                overflow-=1
+                return None
+        except Exception:
+            print(traceback.format_exc(),flush=True)
+            print(url,flush=True)
+
+def runinfo() -> None:
+    global overflow
+    global links
+    maxlen = len(links)
+    while len(links) != 0:
+        if overflow < 12:
+            link = str(links.pop())
+            threading.Thread(target=newinfo,args=(link,)).start()
+            print(f'starting Thread with: {link}',flush=True)
+            print(f'products completed: {maxlen-len(links)}/{maxlen}',flush=True)
+        time.sleep(uniform(0.15,1.5))
+    
+def getlinks():
+    print('started links collection',flush=True)
+    global links
+    r = requests.get('https://deliherb.ru/sitemap.xml')
+    soup = BeautifulSoup(r.text,'xml')
+    
+    products = [i.text for i in soup.find_all('loc') if '/products/' in i.text]
+    links = products
+    print(len(links),flush=True)
+
+def getbadlinks():
+    global links
+    with Session(engine) as session:
+        urls = session.query(Offer.url).where(Offer.barcode == '').all()
+        links = urls
+        print(len(links),flush=True)
+
+def getports():
+    global ports
+    while True:
+        try:
+            client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            client.connect(('127.0.0.1',2501))
+            data = client.recv(4096)
+            ports = ujson.loads(data.decode())
+            client.close()
+            print(f'known ports: {ports}')
+            time.sleep(1)
+        except Exception:
+            print(traceback.print_exc())
+    
+
+            
+        
